5f39a37c1dab559098beeac156ef63de565cc5fa
Giso Grimm <giso.grimm@vegri.net>
2020-10-18 20:30:26 +0200

    add start script

---
+++ startservice.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Usage: ./startservice.sh <numclients> <startport>
+NUMCLIENTS=$1
+STARTPORT=$2
+test -z "${NUMCLIENTS}" && NUMCLIENTS=1
+test -z "${STARTPORT}" && STARTPORT=4366
+echo "starting ${NUMCLIENTS} services starting at port ${STARTPORT}"
+hst=$(hostname)
+k=1
+while test $k -le ${NUMCLIENTS}; do
+    let p=$k+${STARTPORT}
+    let p=$p-1
+    ./build/ov-server -l http://oldbox.orlandoviols.com  -p $p -n ${hst}${k} &
+    sleep 4
+    let k=$k+1
+done
